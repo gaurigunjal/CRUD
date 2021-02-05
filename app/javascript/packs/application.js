aa2c1b5aeaee0612c9c619f7622a86841add9a95
gaurigunjal <gaurigunjal19@gmail.com>
2021-02-05 21:58:45 +0530

    Clean up layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+
+//= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
 Rails.start()
 Turbolinks.start()
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 20}
-    validates :desc, presence: true
+    validates :desc, presence: true, length: { minimum: 3, maximum: 200}
 end
